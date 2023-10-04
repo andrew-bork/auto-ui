ebdcf2cbd7e5205fddf9f50d643290c14859f190
bork <bork.andrew.ryan@gmail.com>
2023-10-03 23:42:33 -0700

    Put color scheme data

---
+++ app/widgets/WidgetSettings.tsx
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+export const Fonts = {
+    primary: "",
+}
+
+export const Colors = {
+    orange: "#E5A355",
+    red: "#E05C5C",
+    green: "#5EE05C",
+    aqua: "#5CE0E0",
+    indigo: "#665CE0",
+    pink: "#E05CCB",
+    white: "#FFFFFF",
+    purple: "#9356F5",
+    blue: "#5679F5"
+}
+
+// export function getShade(color, brightness)
